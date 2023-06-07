a2dc7515d53fa84cf7a893776686903fe0217bf0
Shane-Donlon <shane.donlon@live.com>
2023-06-07 12:43:20 +0100

    Script Add: Button event listener defaults

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,18 @@
+// Wait for dom to load then run the game
+document.addEventListener("DOMContentLoaded", () => {
+  let buttons = document.querySelectorAll("button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (button.getAttribute("data-type" === "submit")) {
+        alert("you clicked submit");
+      } else {
+        let gameType = button.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+      }
+    });
+  });
+});
+
 function runGame() {}
 
 function checkAnswer() {}
@@ -11,4 +26,4 @@ function incrementWrongAnswer() {}
 function displayAdditionQuestion() {}
 function displaySubtractQuestion() {}
 function displayMultiplyQuestion() {}
-function displayDivisionQuestion() {}
+function displayDivisiongQuestion() {}
