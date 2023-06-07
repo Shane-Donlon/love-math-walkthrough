a077389d6b1ef7deafc0f6304739d05eee59bc7e
Shane-Donlon <shane.donlon@live.com>
2023-06-07 12:58:29 +0100

    Script Add: addition game create

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,13 +7,25 @@ document.addEventListener("DOMContentLoaded", () => {
         alert("you clicked submit");
       } else {
         let gameType = button.getAttribute("data-type");
-        alert(`You clicked ${gameType}`);
+        runGame(`${gameType}`);
       }
     });
   });
 });
+/** The main game "loop  called when the script is first loaded"
+ * and after the users' answer proceeds
+ */
+function runGame(gameType) {
+  let num1 = Math.floor(Math.random() * 25) + 1;
+  let num2 = Math.floor(Math.random() * 25) + 1;
 
-function runGame() {}
+  if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+  } else {
+    alert(`unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting`;
+  }
+}
 
 function checkAnswer() {}
 
@@ -23,7 +35,14 @@ function incrementScore() {}
 
 function incrementWrongAnswer() {}
 
-function displayAdditionQuestion() {}
+function displayAdditionQuestion(operand1, operand2) {
+  document.querySelector(".operand1").textContent = operand1;
+  document.querySelector(".operand2").textContent = operand2;
+  let operator = (document.querySelector(".operator").textContent = "+");
+  console.log(operand1, "op1");
+  console.log(operand2, "op2");
+  console.log(operator, "operator");
+}
 function displaySubtractQuestion() {}
 function displayMultiplyQuestion() {}
 function displayDivisiongQuestion() {}
