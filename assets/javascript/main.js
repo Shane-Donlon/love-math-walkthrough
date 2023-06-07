972bf7de9f77781eb59e2a2465443c4c0aade352
Shane-Donlon <shane.donlon@live.com>
2023-06-07 13:32:13 +0100

    Script add: check user answer if correct

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       if (button.getAttribute("data-type" === "submit")) {
-        alert("you clicked submit");
+        checkAnswer();
       } else {
         let gameType = button.getAttribute("data-type");
         runGame(`${gameType}`);
@@ -21,15 +21,52 @@ function runGame(gameType) {
 
   if (gameType === "addition") {
     displayAdditionQuestion(num1, num2);
+  } else if (gameType === "subtraction") {
+    displaySubtractQuestion(num1, num2);
+  } else if (gameType === "multiply") {
+    displayMultiplyQuestion(num1, num2);
+  } else if (gameType === "divide") {
+    displayDivideQuestion(num1, num2);
+  } else if (gameType === "submit") {
+    checkAnswer();
   } else {
     alert(`unknown game type: ${gameType}`);
     throw `Unknown game type: ${gameType}. Aborting`;
   }
 }
 
-function checkAnswer() {}
+function checkAnswer() {
+  let userAnswer = parseInt(document.querySelector(".answer-box").value);
+  let calculatedAnswer = calculateCorrectAnswer();
+  let isCorrect = userAnswer === calculatedAnswer[0];
+  if (isCorrect) {
+    alert("Hey! you got it right! :D");
+  } else {
+    alert(
+      `Awh you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}`
+    );
+  }
+  runGame(calculatedAnswer[1]);
+}
 
-function calculateCorrectAnswer() {}
+function calculateCorrectAnswer() {
+  let operand1 = parseInt(document.querySelector(".operand1").innerText);
+  let operand2 = parseInt(document.querySelector(".operand2").innerText);
+  let operator = document.querySelector(".operator").innerText;
+
+  if (operator === "+") {
+    return [operand1 + operand2, "addition"];
+  } else if (operator === "-") {
+    return [operand1 - operand2, "subtraction"];
+  } else if (operator === "x") {
+    return [operand1 * operand2, "multiply"];
+  } else if (operator === "/") {
+    return [operand1 / operand2, "divide"];
+  } else {
+    alert(`Unimplemented opertator ${operator}`);
+    throw `Unimplemented opertator ${operator}.Aborting`;
+  }
+}
 
 function incrementScore() {}
 
@@ -43,6 +80,27 @@ function displayAdditionQuestion(operand1, operand2) {
   console.log(operand2, "op2");
   console.log(operator, "operator");
 }
-function displaySubtractQuestion() {}
-function displayMultiplyQuestion() {}
-function displayDivisiongQuestion() {}
+function displaySubtractQuestion(operand1, operand2) {
+  document.querySelector(".operand1").textContent = operand1;
+  document.querySelector(".operand2").textContent = operand2;
+  let operator = (document.querySelector(".operator").textContent = "-");
+  console.log(operand1, "op1");
+  console.log(operand2, "op2");
+  console.log(operator, "operator");
+}
+function displayMultiplyQuestion(operand1, operand2) {
+  document.querySelector(".operand1").textContent = operand1;
+  document.querySelector(".operand2").textContent = operand2;
+  let operator = (document.querySelector(".operator").textContent = "x");
+  console.log(operand1, "op1");
+  console.log(operand2, "op2");
+  console.log(operator, "operator");
+}
+function displayDivideQuestion(operand1, operand2) {
+  document.querySelector(".operand1").textContent = operand1;
+  document.querySelector(".operand2").textContent = operand2;
+  let operator = (document.querySelector(".operator").textContent = "/");
+  console.log(operand1, "op1");
+  console.log(operand2, "op2");
+  console.log(operator, "operator");
+}
